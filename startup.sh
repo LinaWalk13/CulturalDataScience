4b923be28f42be9cf4cde170ace825f92d87d361
LinaWalk13 <walkowiaklina@gmail.com>
2024-09-18 16:24:27 +0200

    added the two exercises

---
+++ startup.sh
@@ -0,0 +1,9 @@
+!/usr/bin/env bash
+
+# any python packages you want to have preinstalled
+pip install numpy pandas matplotlib seaborn statistics math pingouin scipy statsmodels scikit-learn
+
+
+# your git config information
+git config --global user.email "walkowiaklina@gmail.com"
+git config --global user.name "LinaWalk13"
